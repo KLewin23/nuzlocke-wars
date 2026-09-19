9d84194662ebd1b334e9149624d5cc2f84364f4e
Kieran Lewin <kieran.lewin2000@gmail.com>
2026-09-19 05:52:01 +0100

    feat(navbar): added placeholder nav bar and background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,69 +1,33 @@
-import Image from "next/image";
+import Image from 'next/image';
+import Icon from '@public/icon.png';
+import TextLogo from '@public/TextLogo.png';
 
 export default function Home() {
-  return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert h-5 w-[100px]"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the{" "}
-            <code className="rounded bg-black/[.06] px-1.5 py-0.5 font-mono text-[0.9em] dark:bg-white/[.08]">
-              page.tsx
-            </code>{" "}
-            file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert h-[14px] w-4"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={14}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
-  );
+	return (
+		<div className="col flex-1 items-center justify-center bg-linear-to-t from-[#242642] to-[#05041b] font-sans">
+			<nav className="row w-full items-center justify-center gap-10 py-6">
+				<div className="row max-w-lg gap-1">
+					{/* <Image src={Icon} alt="Nuzlocke wars icon" height={40} /> */}
+					{/* <Image src={TextLogo} alt="Nuzlocke wars logo" height={40} /> */}
+
+          <p className="text-5xl text-[#ffcc03] drop-shadow-[0_4.2px_1.2px_#356abc]">Nuzlocke Wars</p>
+				</div>
+				<div className="row gap-5">
+					<a href="/">
+						<p className="text-3xl text-white">Home</p>
+					</a>
+					<a href="/">
+						<p className="text-3xl text-white">Participants</p>
+					</a>
+					<a href="/">
+						<p className="text-3xl text-white">Highlights</p>
+					</a>
+					<a href="/">
+						<p className="text-3xl text-white">FAQ</p>
+					</a>
+				</div>
+			</nav>
+			<main className="col w-full max-w-3xl flex-1 items-center justify-between px-16 py-32 sm:items-start"></main>
+		</div>
+	);
 }
